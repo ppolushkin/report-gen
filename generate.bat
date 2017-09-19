82a06a49f5c8ac51d34d5842ae63417e52745d73
Pavel Polushkin <Pavel.Polushkin@sidenis.com>
2017-09-19 12:13:17 +0300

    Fix generate.bat

--- generate.bat
+++ generate.bat
@@ -1,1 +1,1 @@
-jre1.8.0_131\bin\java -jar report-generator-1.0.jar --spring.config.location=application.properties
+jre1.8.0_131\bin\java -jar report-generator-1.0.jar --spring.config.location=application.properties  > logs.txt 2>&1
